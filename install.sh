77b563780d05b94daa62eb09314c066704bca9f5
Ethereal973 <42090277+Ethereal973@users.noreply.github.com>
2018-08-04 19:15:05 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,24 +8,25 @@ server_setup() {
 
 initial() {
     echo "Updating your server!"
-    sudo apt-get -y update 
-    sudo apt-get -y upgrade
-    sudo apt-get -y autoremove
-    sudo apt-get -y autoclean
+    apt -y update 
+    apt -y upgrade
+    apt -y autoremove
+    apt -y autoclean
 }
 
 install_dependencies(){
     echo "Installing dependencies"
-    apt-get -y install software-properties-common
+    apt -y install software-properties-common
     LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php
     add-apt-repository -y ppa:chris-lea/redis-server
-    apt-get update
+    apt -y update
+    apt -y upgrade
     apt -y install php7.2 php7.2-cli php7.2-gd php7.2-mysql php7.2-pdo php7.2-mbstring php7.2-tokenizer php7.2-bcmath php7.2-xml php7.2-fpm php7.2-curl php7.2-zip nginx curl tar unzip git redis-server
 }
 
 install_mariadb() {
-    echo "Installing Mariadb Server."
-    apt-get install software-properties-common
+    echo "Installing Mariadb Server v10.3 - Ubuntu 18.04."
+    apt install software-properties-common
     apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
     add-apt-repository 'deb [arch=amd64,arm64,ppc64el] http://nyc2.mirrors.digitalocean.com/mariadb/repo/10.3/ubuntu bionic main'
     rootpasswd=$(openssl rand -base64 12)
