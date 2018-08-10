b25cc966874429983a740df154648f6e0fb0a648
Ethereal973 <42090277+Ethereal973@users.noreply.github.com>
2018-08-10 17:05:25 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -40,6 +40,9 @@ server_extra() {
     output "Adding repositories and PPAs"
     LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php
     add-apt-repository -y ppa:chris-lea/redis-server
+    sudo apt-get -y install software-properties-common
+    sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
+    sudo add-apt-repository -y 'deb [arch=amd64,arm64,ppc64el] http://nyc2.mirrors.digitalocean.com/mariadb/repo/10.3/ubuntu bionic main'
 
 install_nginx() {
     output "Installing Nginx server."
